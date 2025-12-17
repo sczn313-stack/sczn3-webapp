a5b370a158cd13ca0dee6e461b7ec13b8fb47b28
sczn313-stack <sczn313@gmail.com>
2025-12-17 08:37:37 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+
+  // Dev server (local)
   server: {
+    host: true,
     port: 5173,
+    allowedHosts: [".onrender.com"],
+  },
+
+  // Preview server (what you're running on Render)
+  preview: {
+    host: true,
+    allowedHosts: [".onrender.com"],
   },
 });
